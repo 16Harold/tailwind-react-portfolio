1a6cd74d7b587df0cf796d3ee998cb834177996e
Harold Lopez <116066307+16Harold@users.noreply.github.com>
2023-01-27 16:55:40 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
               <h2 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Harold Lopez</h2>
               <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
               <p className='text-[#18b8c1] py-4 max-w-[700px]'>I'm a fullstack developer specializing in building (and occasionally designing) digital experiences. Currently, I'm focused on building responsive full-stack web application</p>
-              <div>
-                <button className='text-[#ccd6f6] border-2 border-[#18b8c1] py-2 px-3 my-2 flex items-center hover:bg-[#18b8c1] hover:border-[#ccd6f6] '>View Work</button>
-              </div>
+//               <div>
+//                 <button className='text-[#ccd6f6] border-2 border-[#18b8c1] py-2 px-3 my-2 flex items-center hover:bg-[#18b8c1] hover:border-[#ccd6f6] '>View Work</button>
+//               </div>
          </div>
 
     </div>
